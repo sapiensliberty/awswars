3886090906b9e207dd3be5c553e45e0260186232
sapiensliberty <elqtodolov@gmail.com>
2021-06-12 15:20:26 -0500

    No te pases TCS
    No te pases TCS

--- esquemapersonas.js
+++ esquemapersonas.js
@@ -1,6 +1,6 @@
 const faunadb = require('faunadb');
 const q = faunadb.query;
-const client = new faunadb.Client({ secret: 'fnAEIe5MCIACDZUAnLENAT3vC_ICGCpmiPhuLwh7' });
+const client = new faunadb.Client({ secret: 'faunasecretkey' });
 const clc="personas";
 const idc="idc_personas";
 
